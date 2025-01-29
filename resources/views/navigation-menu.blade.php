0d3b9037fca883d2dc3da9396f32ad6f1b39ce06
Abhijit Roy <abhijit.r@nic.in>
2025-01-29 15:21:41 +0530

    Product link added

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ url('products') }}">
+                        {{ __('Product') }}
+                    </x-nav-link>
                 </div>
             </div>
 
